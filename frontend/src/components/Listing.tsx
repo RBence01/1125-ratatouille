3a6fd0ca1915f514df7976a60950a9d0030495b7
Petrik Diak <diak@petrik.hu>
2024-11-25 09:56:09 +0100

    Listing component

---
+++ frontend/src/components/Listing.tsx
@@ -0,0 +1,104 @@
+import { useEffect, useState } from "react";
+import { chefRat } from "../Types";
+import "../css/Listing.css";
+
+export default function Listing({ pagesOn = false, searchOn = false, orderOn = false }: { pagesOn?: boolean, searchOn?: boolean, orderOn?: boolean }) {
+    const [data, setData] = useState<{ data: chefRat[], pages: number } | null>(null);
+    const [sortData, setSortData] = useState<{ col: string, direction: string } | undefined>(undefined);
+    const [searchQuery, setSearchQuery] = useState<string | undefined>(undefined);
+    const [page, setPage] = useState<number>(1);
+    useEffect(() => {
+        load(5, page, searchQuery, sortData?.col, sortData?.direction);
+        console.log('loaded')
+    }, [sortData, searchQuery, page]);
+
+    function search(event: React.ChangeEvent<HTMLInputElement>) {
+        setPage(1);
+        setSearchQuery(event.target.value);
+    }
+
+    async function load(limit?: number, page?: number, search?: string, orderBy?: string, direction?: string,) {
+        const params: any = {};
+        if (searchOn && search) params.search = search;
+        if (pagesOn && limit) params.limit = limit;
+        if (pagesOn && page) params.page = page
+        if (orderOn && orderBy) params.groupby = orderBy;
+        if (orderOn && direction) params.order = direction;
+        const response = await fetch('http://localhost:3000/rats?' + new URLSearchParams(params));
+        setData(await response.json());
+    }
+
+    function sort(event: any) {
+        if (!orderOn) return;
+        let direction;
+        const dir = event.target.classList.contains('ASC');
+        for (let e of document.getElementsByClassName('DESC')) {
+            e.classList.remove('DESC');
+        }
+        for (let e of document.getElementsByClassName('ASC')) {
+            e.classList.remove('ASC');
+        }
+        if (dir) {
+            direction = 'DESC';
+            event.target.classList.add('DESC');
+        }
+        else {
+            direction = 'ASC';
+            event.target.classList.add('ASC');
+        }
+        setPage(1);
+        setSortData({ col: event.target.dataset.name, direction });
+    }
+
+    function changePage(event: any) {
+        const pageValue = event.target.dataset.page;
+        if (pageValue == "b") setPage(Math.max(1, page - 1));
+        else if (pageValue == "f") setPage(Math.min(data!.pages, page + 1));
+        else setPage(pageValue);
+    }
+
+    let pages;
+    if (data?.pages) {
+        pages = [];
+        for (let i = 1; i <= data?.pages; i++) pages.push(i);
+    }
+
+    return <>
+        <div className="container">
+            {searchOn && (
+                <div className="search">
+                    <input type="text" name="search" onChange={search} />
+                    <div></div>
+                </div>)}
+            <table>
+                <thead>
+                    <tr>
+                        <th className={orderOn ? "sort" : ""} onClick={sort} data-name={"ranking"}>Ranking</th>
+                        <th className={orderOn ? "sort" : ""} onClick={sort} data-name={"species"}>Species</th>
+                        <th className={orderOn ? "sort" : ""} onClick={sort} data-name={"name"}>Name</th>
+                        <th className={orderOn ? "sort" : ""} onClick={sort} data-name={"job"}>Job</th>
+                        <th className={orderOn ? "sort" : ""} onClick={sort} data-name={"special_dish"}>Special Dish</th>
+                        <th className={orderOn ? "sort" : ""} onClick={sort} data-name={"height"}>Height</th>
+                        <th className={orderOn ? "sort" : ""} onClick={sort} data-name={"salary"}>salary</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data && data.data.map((e) => <tr key={e.id}>
+                        <td>{e.ranking}</td>
+                        <td>{e.species}</td>
+                        <td>{e.name}</td>
+                        <td>{e.job}</td>
+                        <td>{e.specialDish}</td>
+                        <td>{e.height}</td>
+                        <td>{e.salary}</td>
+                    </tr>)}
+                </tbody>
+            </table>
+            {pagesOn && pages && <div className="pages">
+                <button data-page="b" onClick={changePage}>←</button>
+                {pages.map(e => <button key={e} data-page={e} onClick={changePage}>{e}</button>)}
+                <button data-page="f" onClick={changePage}>→</button>
+            </div>}
+        </div>
+    </>
+} 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './pages/Layout'
+import Listing from './components/Listing'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
     <Routes>
         <Route path='/' element={<Layout/>}>
-          <Route index element={<h1>home</h1>}/>
+          <Route index element={<h1>Home</h1>} />
+          <Route path='rats' element={<Listing/>} />
         </Route>
     </Routes>
     </BrowserRouter>
