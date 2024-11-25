1c52b19a1579f340dc0323c906ce2571a73978c9
AgnesFolytan <folytanagnes@gmail.com>
2024-11-25 10:38:21 +0100

    nav done

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -17,7 +17,7 @@ createRoot(document.getElementById('root')!).render(
           <Route path='search' element={<Listing searchOn/>} />
           <Route path='order' element={<Listing orderOn/>} />
           <Route path='pages' element={<Listing pagesOn/>} />
-
+          <Route path='delete' element={<Listing deleteOn/>} />
         </Route>
     </Routes>
     </BrowserRouter>
