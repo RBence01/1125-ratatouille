4f923a2f289c5eecde3692ab847f6467540f8252
Petrik Diak <diak@petrik.hu>
2024-11-27 09:03:00 +0100

    sql

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -13,15 +13,11 @@ createRoot(document.getElementById('root')!).render(
         <Route path='/' element={<Layout/>}>
           <Route index element={<h1>Home</h1>} />
           <Route path='new' element={<NewData/>} />
-<<<<<<< HEAD
-          <Route path='list' element={<Listing searchOn orderOn pagesOn/>} />
-=======
           <Route path='list' element={<Listing/>} />
           <Route path='search' element={<Listing searchOn/>} />
           <Route path='order' element={<Listing orderOn/>} />
           <Route path='pages' element={<Listing pagesOn/>} />
           <Route path='delete' element={<Listing deleteOn/>} />
->>>>>>> 1c52b19a1579f340dc0323c906ce2571a73978c9
         </Route>
     </Routes>
     </BrowserRouter>
